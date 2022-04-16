502bd35653a8120c42edf425b8e33a2aa735a195
cyane-github <cyane.email@protonmail.com>
2022-04-16 07:37:44 +0200

    timer element it now added to turn order window

--- timer.js
+++ timer.js
@@ -18,19 +18,42 @@ const waitForSelector = (selector) => {
   })
 }
 
+// <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><span id="ui-id-9" class="ui-dialog-title">Turn timer 1:06</span></div>
+
 let previouslyFirstInTurnOrder = null
-const timerObserverCallback = async (mutationsList) => {
+const characterListObserverCallback = async (mutationsList) => {
   const firstInTurnOrder = await waitForSelector('#initiativewindow .characterlist > li:nth-child(1)').then(element => element.outerHTML)
 
   mutationsList.forEach(() => {
     if (previouslyFirstInTurnOrder !== firstInTurnOrder) {
+      console.log('roll20-made-easlier detected a new turn')
       previouslyFirstInTurnOrder = firstInTurnOrder
     }
   })
 }
 
-const timerObserver = new MutationObserver(timerObserverCallback)
+const characterListObserver = new MutationObserver(characterListObserverCallback)
 
-waitForSelector('.characterlist').then(targetNode => {
-  timerObserver.observe(targetNode, { childList: true })
+waitForSelector('#initiativewindow').then(async initiativeWindow => {
+  const timerWrapperElement = document.createElement('div')
+  timerWrapperElement.setAttribute('class', 'roll20-made-easlier-timer-wrapper ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix')
+
+  const timerElement = document.createElement('span')
+  timerElement.setAttribute('id', 'ui-id-9')
+  timerElement.setAttribute('class', 'roll20-made-easlier-timer ui-dialog-title')
+
+  const timerTextNode = document.createTextNode('Turn timer 00:00')
+
+  timerElement.appendChild(timerTextNode)
+  timerWrapperElement.appendChild(timerElement)
+
+  const turnOrderWindow = await waitForSelector('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.initiativedialog.ui-draggable.ui-resizable.ui-dialog-buttons')
+  turnOrderWindow.insertBefore(timerWrapperElement, initiativeWindow)
 })
+
+waitForSelector('.characterlist').then(characterList => {
+  characterListObserver.observe(characterList, { childList: true })
+})
+
+// TODO: Add round number and in-game timer. E.g. "Round: 7 (1m 06s)"
+// TODO: Add in-game timer. E.g. "In-game time: 6 * (number of rounds)"
